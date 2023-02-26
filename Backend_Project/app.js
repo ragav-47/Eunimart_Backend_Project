7c35d461f15d717b3a9740aa95c4655fe3df8af0
Vijayaragavan <75235488+ragav-47@users.noreply.github.com>
2023-02-26 16:47:45 +0530

    Update app.js

--- Backend_Project/app.js
+++ Backend_Project/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use("/api/user",router);
 mongoose
    .connect(
-     "mongodb+srv://Admin:IYp3vlvpin3t3WHZ@cluster0.uzfaqod.mongodb.net/Blog?retryWrites=true&w=majority"
+     "mongodb+srv://<username>:<password>@cluster0.uzfaqod.mongodb.net/Blog?retryWrites=true&w=majority"
 )
 .then(()=>app.listen(5000))
 .then(()=>console.log("Connected To database"))
